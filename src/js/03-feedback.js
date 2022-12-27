61288e7dceff48ac6b34bc49fde6af8ae2967e45
PavloZiuskin <passzuskin@gmail.com>
2022-12-27 13:42:45 +0200

    fix-hw-task-3-again

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,47 +8,44 @@ const refs = {
   textarea: document.querySelector('.feedback-form textarea'),
 };
 
-const formObj = {};
+let formObj = {
+  email: '',
+  textarea: '',
+};
+refs.input.value = formObj.email;
+refs.textarea.textContent = formObj.textarea;
 
-refs.input.addEventListener(
+refs.form.addEventListener(
   'input',
   _.throttle(e => {
     onFormStateInput(e);
   }, 500)
 );
-refs.textarea.addEventListener(
-  'input',
-  _.throttle(e => {
-    console.log(e.target.value);
-    onFormStateTextarea(e);
-  }, 500)
-);
+
 refs.form.addEventListener('submit', onClearLocalSubmit);
 
 function onFormStateInput(e) {
   const formCurrentTarget = e.target;
-
-  (formObj.email = formCurrentTarget.value), addLockal(formObj);
-}
-function onFormStateTextarea(e) {
-  const formCurrentTarget = e.target;
-
-  (formObj.message = formCurrentTarget.value), addLockal(formObj);
-}
-
-function addLockal(obj) {
-  localStorage.setItem(FORM_FEEDBACK_LOKAL_KEY, JSON.stringify(obj));
+  if (formCurrentTarget === refs.input) {
+    formObj.email = formCurrentTarget.value;
+    localStorage.setItem(FORM_FEEDBACK_LOKAL_KEY, JSON.stringify(formObj));
+  } else if (formCurrentTarget === refs.textarea) {
+    formObj.textarea = formCurrentTarget.value;
+    localStorage.setItem(FORM_FEEDBACK_LOKAL_KEY, JSON.stringify(formObj));
+  }
 }
 
 function autoComplite() {
   const readLocal = localStorage.getItem(FORM_FEEDBACK_LOKAL_KEY);
-  const newObj = JSON.parse(readLocal);
+  const newobj = JSON.parse(readLocal);
 
-  if (!newObj) {
+  if (!newobj) {
     return;
   }
-  refs.input.value = newObj.email;
-  refs.textarea.textContent = newObj.message;
+  refs.input.value = newobj.email;
+  refs.textarea.textContent = newobj.textarea;
+  formObj.email = newobj.email;
+  formObj.textarea = newobj.textarea;
 }
 
 if (!refs.input.value || !refs.textarea.value) {
@@ -57,6 +54,10 @@ if (!refs.input.value || !refs.textarea.value) {
 
 function onClearLocalSubmit(e) {
   e.preventDefault();
-  console.log(formObj);
-  localStorage.removeItem(FORM_FEEDBACK_LOKAL_KEY);
+  if (formObj.email !== '' && formObj.textarea !== '') {
+    console.log(formObj);
+    localStorage.removeItem(FORM_FEEDBACK_LOKAL_KEY);
+    formObj.email = '';
+    formObj.textarea = '';
+  }
 }
